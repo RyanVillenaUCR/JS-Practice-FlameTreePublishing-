7369eb1a3eb736eeb849619d5b08bfa2f41e1116
RyanVillenaUCR <rvill028@ucr.edu>
2021-03-13 17:23:31 -0800

    Moved startup from HTML to startup script

---
+++ SimpleQuiz/js/quizStartup.js
@@ -0,0 +1,14 @@
+var com;
+if (!com)
+    com = {};
+
+if (!com.flametreepublishing)
+    com.flametreepublishing = {};
+
+com.flametreepublishing.startQuiz = function() {
+
+    com.flametreepublishing.simpleQuiz = new com.flametreepublishing.SimpleQuiz();
+    com.flametreepublishing.simpleQuiz.renderAllQuestions();
+}
+
+window.addEventListener("load", com.flametreepublishing.startQuiz, false);
